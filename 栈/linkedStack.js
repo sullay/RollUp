77ed486866cf46c233cfb48ba7e435011df12568
sullayang(杨金伟) <sullayang@futunn.com>
2021-12-22 16:15:09 +0800

    feat: 链表栈基本操作

---
+++ 栈/linkedStack.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(element) {
+    this.element = element
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null
+    this.length = 0
+  }
+
+  push(element) {
+    let node = new Node(element)
+    if (this.top) {
+      node.next = this.top
+      this.top = node
+    } else {
+      this.top = node
+    }
+    this.length++
+  }
+
+  pop() {
+    if (!this.top) {
+      return -1
+    }
+    let temp = this.top.element
+    this.top = this.top.next
+    this.length--
+    return temp
+  }
+
+  clear() {
+    this.top = null
+    this.length = 0
+  }
+
+  display() {
+    let temp = this.top
+    while (temp) {
+      console.log(temp.element)
+      temp = temp.next
+    }
+  }
+}
+
+let stack = new Stack();
+
+stack.push('a')
+stack.push('b')
+stack.push('c')
+
+stack.display()
+console.log(stack.length)
+// console.log(stack.pop(), stack.length)
+// console.log(stack.pop(), stack.length)
+// console.log(stack.pop(), stack.length)
+// console.log(stack.pop(), stack.length)
+// stack.display()
